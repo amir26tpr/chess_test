2d89cce2987ff3d37672b3e07f7436c766451a52
amirseigal <amirreza592@email.com>
2023-06-30 13:50:53 +0330

    moves

--- Chess.py
+++ Chess.py
@@ -1,65 +1,70 @@
 class ChessPiece:
     def __init__(self, color):
         self.color = color
-        
     
     def can_move(self, start, end, board):
-        # Logic to determine if the piece can move from 'start' to 'end'
         raise NotImplementedError("Subclasses must implement the 'can_move' method.")
 
-
     def __str__(self):
-        # String representation of the chess piece
         raise NotImplementedError("Subclasses must implement the '__str__' method.")
     
 
-    class King(ChessPiece):
-        def can_move(self, start, end, board):
+class King(ChessPiece):
+    def can_move(self, start, end, board):
+        start_row, start_col = start
+        end_row, end_col = end
+        row_diff = abs(start_row - end_row)
+        col_diff = abs(start_col - end_col)
+        return row_diff <= 1 and col_diff <= 1 and (board[end_row][end_col] is None or board[end_row][end_col].color != self.color)
+    
+    def __str__(self):
+         
+
+class Queen(ChessPiece):
+    def can_move(self, start, end, board):
+        start_row, start_col = start
+        end_row, end_col = end
+        row_diff = abs(start_row - end_row)
+        col_diff = abs(start_col - end_col)
+        return (start_row == end_row or start_col == end_col or row_diff == col_diff) and (board[end_row][end_col] is None or board[end_row][end_col].color != self.color)
+
+    def __str__(self):
 
 
-
-        def __str__(self):
-
-
-
-
-    class Queen(ChessPiece):
-        def can_move(self, start, end, board):
-
-
-
-        def __str__(self):
+class Bishop(ChessPiece):
+    def can_move(self, start, end, board):
+        start_row, start_col = start
+        end_row, end_col = end
+        row_diff = abs(start_row - end_row)
+        col_diff = abs(start_col - end_col)
+        return row_diff == col_diff and (board[end_row][end_col] is None or board[end_row][end_col].color != self.color)
+    
+    def __str__(self):
 
 
+class Rook(ChessPiece):
+    def can_move(self, start, end, board):
+        start_row, start_col = start
+        end_row, end_col = end
+        return start_row == end_row or start_col == end_col and (board[end_row][end_col] is None or board[end_row][end_col].color != self.color)
 
-    class Bishop(ChessPiece):
-        def can_move(self, start, end, board):
-
-
-
-        def __str__(self):
-
-
-    class Rook(ChessPiece):
-        def can_move(self, start, end, board):
+    def __str__(self):
 
 
-
-        def __str__(self):
-
-
-
-    class Knight(ChessPiece):
-        def can_move(self, start, end, board):
-
-
-
-        def __str__(self):
+class Knight(ChessPiece):
+    def can_move(self, start, end, board):
+        start_row, start_col = start
+        end_row, end_col = end
+        row_diff = abs(start_row - end_row)
+        col_diff = abs(start_col - end_col)
+        return (row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2) and (board[end_row][end_col] is None or board[end_row][end_col].color != self.color)
+     
+    def __str__(self):
 
 
-    class Pawn(ChessPiece):
-        def can_move(self, start, end, board):
+class Pawn(ChessPiece):
+    def can_move(self, start, end, board):
+        
 
 
-
-        def __str__(self):
+    def __str__(self):
