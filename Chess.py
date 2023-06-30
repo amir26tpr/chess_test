a0b46bf2ac60de0bad9c100cf05f711a34e3a5ed
amir26tpr <amirtaleshpour@gmail.com>
2023-06-30 13:33:35 +0330

    can_move & str

--- Chess.py
+++ Chess.py
@@ -2,3 +2,12 @@ class ChessPiece:
     def __init__(self, color):
         self.color = color
         
+    
+    def can_move(self, start, end, board):
+        # Logic to determine if the piece can move from 'start' to 'end'
+        raise NotImplementedError("Subclasses must implement the 'can_move' method.")
+
+
+    def __str__(self):
+        # String representation of the chess piece
+        raise NotImplementedError("Subclasses must implement the '__str__' method.")
