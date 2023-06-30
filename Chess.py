f10f74efdf9cdbf8fe821e8c0081acb7dad05666
amir26tpr <amirtaleshpour@gmail.com>
2023-06-30 13:39:51 +0330

    define chesspiece classes

--- Chess.py
+++ Chess.py
@@ -11,3 +11,55 @@ class ChessPiece:
     def __str__(self):
         # String representation of the chess piece
         raise NotImplementedError("Subclasses must implement the '__str__' method.")
+    
+
+    class King(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
+
+
+
+
+    class Queen(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
+
+
+
+    class Bishop(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
+
+
+    class Rook(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
+
+
+
+    class Knight(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
+
+
+    class Pawn(ChessPiece):
+        def can_move(self, start, end, board):
+
+
+
+        def __str__(self):
